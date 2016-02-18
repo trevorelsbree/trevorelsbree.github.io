1d344b75ba47dbab5a21027e69f2dcf25dac2bd7
trevorelsbree <trevorlelsbree@gmail.com>
2016-02-18 11:39:39 -0800

    Added basic functionality
    Site navigator, about me info, basic css

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy; " + new Date().getFullYear() + " Trevor Elsbree. All rights reserved. </p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
